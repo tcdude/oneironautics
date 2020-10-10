c9e543e88936113d407acee7dfec56ecc02036c6
janentikan <momojo@rocketship.com>
2020-10-10 02:20:01 +0200

    oops, removed panda3d-keybindings from main

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import sys
 
 from direct.showbase.ShowBase import ShowBase
-from keybindings.device_listener import add_device_listener
-from keybindings.device_listener import SinglePlayerAssigner
 
 import panda3d
 from panda3d.core import WindowProperties
@@ -24,10 +22,6 @@ class GameApp(ShowBase):
         self.disable_mouse()
         self.input = Input()
         self.accept('escape', sys.exit)
-        add_device_listener(
-            config_file='keybindings.toml',
-            assigner=SinglePlayerAssigner(),
-        )
         self.world = World()
         self.set_background_color(util.srgb_color(0x292931))
 
